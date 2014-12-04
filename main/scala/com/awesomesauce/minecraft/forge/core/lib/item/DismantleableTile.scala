0174accfc1f2e8107d6ee6b1a81b29e3710a08e0
gjgfuj <gjgfujb@gmail.com>
2014-12-05 10:20:22 +1100

    Added the CoFH dismantleable API for the machines. - Part 2

--- main/scala/com/awesomesauce/minecraft/forge/core/lib/item/DismantleableTile.scala
+++ main/scala/com/awesomesauce/minecraft/forge/core/lib/item/DismantleableTile.scala
@@ -4,10 +4,21 @@ import java.util
 
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.item.ItemStack
+import net.minecraft.tileentity.TileEntity
 
 
-trait DismantleableTile {
+trait DismantleableTile extends TileEntity {
   def canDismantle(player: EntityPlayer): Boolean
 
   def dismantleTile(player: EntityPlayer, returnDrops: Boolean): util.ArrayList[ItemStack]
+}
+
+trait BasicDismantleableTile extends DismantleableTile {
+  def canDismantle(player: EntityPlayer) = true
+
+  def dismantleTile(player: EntityPlayer, returnDrops: Boolean): util.ArrayList[ItemStack] = {
+    val list = new util.ArrayList[ItemStack]()
+    list.add(new ItemStack(this.blockType))
+    list
+  }
 }
