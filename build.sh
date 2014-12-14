104adb0556bfbce01d44e82d7feb6ec406d65ec1
gjgfuj <gjgfujb@gmail.com>
2014-12-14 20:47:41 +1100

    Added travis-ci.org building.

---
+++ build.sh
@@ -0,0 +1,8 @@
+mkdir build
+mkdir build/src
+git clone https://github.com/AwesomeSauceMods/AwesomeSauceCore.git build/src
+git clone https://github.com/AwesomeSauceMods/CoolInfoStuff.git
+mv CoolInfoStuff/* build
+cd build
+rm -r OpenAutomation
+bash gradlew build
